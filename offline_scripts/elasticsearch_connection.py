22865e62545cf08012e8b51d64581357f77b8658
rsinghal <rashmi.s.singhal@gmail.com>
2015-10-30 13:07:30 -0400

    start saving JSON objects to elastic search.  associate related items from Objects table (and separate them out)

---
+++ offline_scripts/elasticsearch_connection.py
@@ -0,0 +1,41 @@
+from elasticsearch import Elasticsearch
+
+# TODO: this needs to come from settings
+ELASTICSEARCH_URL = 'localhost:9200'
+ELASTICSEARCH_INDEX = 'giza'
+
+# Connect to elasticsearch db
+def get_connection():
+    return Elasticsearch(ELASTICSEARCH_URL)
+
+# Gets the content of an item, returns JSON
+def get_item(item_id, source):
+    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=item_id)["_source"]
+
+# Inserts JSON document into elasticsearch with the given item_id
+# Either adds new document or replaces existing document
+def add_or_update_item(item_id, document, source):
+    es.index(index=ELASTICSEARCH_INDEX, doc_type=source, id=item_id, body=document)
+
+# Checks if item exists in elasticsearch, returns boolean
+def item_exists(item_id, source):
+    return es.exists(index=ELASTICSEARCH_INDEX, doc_type=source, id=item_id)
+
+def get_all_item_ids_with_type(source):
+    results = es.search(index=ELASTICSEARCH_INDEX, doc_type=source, fields="[]")
+    ids = []
+    for r in results["hits"]["hits"]:
+        ids.append(str(r["_id"]))
+    return ids
+
+def get_all_item_ids():
+    results = es.search(index=ELASTICSEARCH_INDEX, fields="[]")
+    ids = []
+    for r in results["hits"]["hits"]:
+        ids.append(str(r["_id"]))
+    return ids
+
+def get_item_title(item_id, source):
+    return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=item_id)["_source"]["label"]
+
+es = get_connection()
--- offline_scripts/sites.py
+++ offline_scripts/sites.py
@@ -4,42 +4,63 @@
 import csv
 import codecs
 import json
+import elasticsearch_connection
+
 from classifications import CLASSIFICATIONS
 
+# First update each Site with the latest data
+# This is the basic information/metadata that comprises a Site
 with open('../data/sites.csv', 'rb') as csvfile:
+	# Get the query headers to use as keys in the JSON
 	headers = next(csvfile)
 	if headers.startswith(codecs.BOM_UTF8):
 		headers = headers[3:]
 	headers = headers.replace('\r\n','')
 	columns = headers.split(',')
-	print columns
+	site_id_index = columns.index('SiteID')
+
 	objects = csv.reader(csvfile, delimiter=',', quotechar='"')
 
 	site = {}
 	current_id = '-1'
 	for row in objects:
-		if row[0] not in ('1','42'):
+		site_id = row[site_id_index]
+		if site_id not in ('1','42','1175'):
 			continue
-		print row[0]
-		if row[0] != current_id:
-			current_id = row[0]
+		# could have multiple rows for one site because of multiple SiteDates or other pieces of information
+		# only create a new site if we have a new site id
+		if site_id != current_id:
+			if site:
+				elasticsearch_connection.add_or_update_item(current_id, json.dumps(site), 'sites')
+			current_id = site_id
 			site = {}
 
-		# could have multiple rows for one site because of multiple SiteDates or other pieces of information
+		# loop through each row
 		for index, value in enumerate(columns):
 			key = value.lower()
+			row_value = row[index]
+
+			# cleanup row data
+			if row_value.isdigit():
+				row_value = int(row_value)
+			elif row_value == "NULL":
+				row_value = None
+
 			if 'sitetype' in key:
+				# group sitetype fields into an object
 				if 'sitetype' not in site:
 					site['sitetype'] = {}
-				site['sitetype'][key] = row[index]
+				site['sitetype'][key] = row_value
 			elif 'sitedates' in key:
+				# there can be multiple sitedates
+				# create an array that contains all sitedate objects
 				if 'sitedates' not in site:
 					site['sitedates'] = []
 				# key looks like 'SiteDates_EventType_DateText'
 				# row data looks like 'PorterMoss Date_Dynasty 5-6'
 				# split on _ (and ignore first value in key)
 				keys = key.split('_')[1:]
-				values = row[index].split('_')
+				values = row_value.split('_')
 				date = {}
 				for i, k in enumerate(keys):
 					if values[i]:
@@ -47,6 +68,61 @@ with open('../data/sites.csv', 'rb') as csvfile:
 				if date:
 					site['sitedates'].append(date)
 			else:
-				site[key] = row[index]
-		print site
+				# no special processing - just add it to the JSON
+				site[key] = row_value
+		#print json.dumps(site)
+	if site:
+		elasticsearch_connection.add_or_update_item(current_id, json.dumps(site), 'sites')
 
+# Then update all related items from the Objects table
+# TODO: Get Primary Image URL (need an image server first) 
+with open('../data/sites_objects_related.csv', 'rb') as csvfile:
+	# Get the query headers to use as keys in the JSON
+	headers = next(csvfile)
+	if headers.startswith(codecs.BOM_UTF8):
+		headers = headers[3:]
+	headers = headers.replace('\r\n','')
+	columns = headers.split(',')
+
+	site_id_index = columns.index('SiteID')
+	classification_id_index = columns.index('ClassificationID')
+	object_id_index = columns.index('ObjectID')
+	object_title_index = columns.index('Title')
+	object_number_index = columns.index('ObjectNumber')
+
+	current_id = '-1'
+	site = {}
+	objects = csv.reader(csvfile, delimiter=',', quotechar='"')
+	for row in objects:
+		site_id = row[site_id_index]
+		if site_id not in ('1','42','1175'):
+			continue
+		if site_id != current_id:
+			if site:
+				elasticsearch_connection.add_or_update_item(current_id, json.dumps(site), 'sites')
+			current_id = site_id
+			site = {}
+			if elasticsearch_connection.item_exists(site_id, 'sites'):
+				site = elasticsearch_connection.get_item(site_id, 'sites')
+			else:
+				print "this site could not be found!"
+				continue
+		if 'relateditems' not in site:
+			site['relateditems'] = {}
+		classification_key = int(row[classification_id_index])
+		classification = CLASSIFICATIONS.get(classification_key)
+		object_id = int(row[object_id_index])
+
+		object_title = row[object_title_index]
+		if classification == "diary pages" and object_title.lower() == "null":
+			object_number = row[object_number_index]
+			idx = object_number.find('_')
+			object_title = object_number[idx+1:]
+		if object_title.lower() == "null":
+			object_title = "[No Title]"
+
+		if classification not in site['relateditems']:
+			site['relateditems'][classification] = []
+		site['relateditems'][classification].append({'objectid' : object_id, 'title' : object_title})
+	if site:
+		elasticsearch_connection.add_or_update_item(current_id, json.dumps(site), 'sites')
