507bf4ab4bbb1978735a1ed331ac89e9168362ea
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2016-09-30 12:05:22 -0400

    add more/less links for constituent descriptions

--- giza/static/js/giza.js
+++ giza/static/js/giza.js
@@ -45,3 +45,36 @@ $('#search-dropdown').on('show.zf.dropdown', function() {
   }, 250);
 });
 
+// from http://jsfiddle.net/iambriansreed/bjdSF/ and http://stackoverflow.com/a/5454303
+$(function() {
+  var minimized_elements = $('span.minimize');
+
+  minimized_elements.each(function(){
+    var t = $(this).text(),
+    max_length = 76;
+
+    if(t.length < max_length) return;
+
+    var trimmed_string = t.substr(0, max_length);
+    max_length = Math.min(trimmed_string.length, trimmed_string.lastIndexOf(" "));
+    trimmed_string = trimmed_string.substr(0, max_length);
+
+    $(this).html(
+      trimmed_string+'<span> ... </span><a href="#" class="more">More</a>'+
+      '<span style="display:none;">'+ t.substr(max_length,t.length)+' <a href="#" class="less">Less</a></span>'
+    );
+
+  });
+
+  $('a.more', minimized_elements).click(function(event){
+    event.preventDefault();
+    $(this).hide().prev().hide();
+    $(this).next().show();
+  });
+
+  $('a.less', minimized_elements).click(function(event){
+    event.preventDefault();
+    $(this).parent().hide().prev().show().prev().show();
+  });
+
+});
