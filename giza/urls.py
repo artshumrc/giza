4b00422db6ec922c550e8681110708b893428e08
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2017-01-04 14:32:39 -0500

    closes #34.  Image display should be better (and more efficient)

--- giza/urls.py
+++ giza/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^search/$', 'search.views.search', name='search'),
     url(r'^search-results/$', 'search.views.results', name='results'),
 
-    url(r'^(?P<type>[0-9a-z]+)/(?P<id>[\d]+)/(?P<view>intro|full)?/$', 'tms.views.get_type_html', name='get_type_html'),
+    url(r'^(?P<type>[0-9a-z]+)/(?P<id>[\d]+)/(?P<view>intro|full|allphotos)?/$', 'tms.views.get_type_html', name='get_type_html'),
     url(r'^(?P<type>[0-9a-z]+)/(?P<id>[\d]+)\.json$', 'tms.views.get_type_json', name='get_type_json'),
 
     url(r'', include(wagtail_urls)),
