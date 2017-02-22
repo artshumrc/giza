4daad98e09483c8ef5c7e6fdbd6c25d9d2a02f27
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2017-02-22 16:59:53 -0500

    add STATIC_ROOT

--- giza/settings.py
+++ giza/settings.py
@@ -141,5 +141,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'http_static')
 STATIC_URL = '/static/'
