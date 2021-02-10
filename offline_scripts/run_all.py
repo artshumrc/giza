41149840d3eb2bb63de6a50cb86f76e1a5d311bb
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2021-02-10 10:28:25 -0500

    add more args to `run_all`

--- offline_scripts/run_all.py
+++ offline_scripts/run_all.py
@@ -16,6 +16,11 @@ def main():
 	parser.add_argument('-u','--username', help='TMS Username', required=False, default='RC\\rsinghal')
 	parser.add_argument('-p','--password', help='TMS Password', required=False)
 	parser.add_argument('-i','--iiif', help='Run IIIF Manifests script', required=False, default=False)
+	parser.add_argument('-s','--sites', help='Run Sites script', required=False, default=True)
+	parser.add_argument('-o','--objects', help='Run Objects script', required=False, default=True)
+	parser.add_argument('-c','--constituents', help='Run Constituents script', required=False, default=True)
+	parser.add_argument('-b','--published', help='Run Published script', required=False, default=True)
+	parser.add_argument('-m','--media', help='Run Media script', required=False, default=True)
 	args = vars(parser.parse_args())
 
 	try:
@@ -36,11 +41,16 @@ def main():
 
 	if args['iiif']:
 		iiif_manifests.main(CURSOR)
-	sites.main(CURSOR)
-	objects.main(CURSOR)
-	constituents.main(CURSOR)
-	published.main(CURSOR)
-	media.main(CURSOR)
+	if args['sites']:
+		sites.main(CURSOR)
+	if args['objects']:
+		objects.main(CURSOR)
+	if args['constituents']:
+		constituents.main(CURSOR)
+	if args['published']:
+		published.main(CURSOR)
+	if args['media']:
+		media.main(CURSOR)
 
 if __name__ == "__main__":
    main()
