e42900a2b71694df1236a36244e409fa275bc9da
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-01-06 14:31:28 -0500

    Update env files

--- es8-ingestion-scripts/credentials_default.py
+++
@@ -1,9 +0,0 @@
-tms_dsn = 'giza-dev.rc.fas.harvard.edu'
-tms_user = 'your username'
-tms_password = 'your password'
-tms_databases = ['gizaCARDTMS_2', 'gizaCARDTMSThes_2'] # THESE ARE THE TWO DATABASE IN TMS
-
-# ES8
-es_user = 'elastic'
-es_password = 'xFH89gDxvMBXdk6M'
-es_cert = 'path to certificates' # How doe I get these from ES8 container?
