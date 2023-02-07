dad3321e9d6d996253e1a1932283df1458fe0fef
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-02-07 18:24:01 -0500

    Remove offline_scripts

--- offline_scripts/credentials_default.py
+++
@@ -1,9 +0,0 @@
-tms_dsn = 'giza-dev.rc.fas.harvard.edu'
-tms_user = 'your username'
-tms_password = 'your password'
-tms_databases = ['gizaCARDTMS_2', 'gizaCARDTMSThes_2'] # THESE ARE THE TWO DATABASE IN TMS
-
-# ES8
-es_user = 'elasticsearch username'
-es_password = 'password here'
-es_cert = 'path to certificates'
