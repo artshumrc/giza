6716995ffab36076fb3b64843f4804069a9e9ba8
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-02-09 15:25:25 -0500

    Remove odbc and freetds config

--- credentials.py
+++
@@ -1,8 +0,0 @@
-tms_dsn = 'giza-dev.rc.fas.harvard.edu'
-tms_user = 'your_user_name_here'
-tms_password = 'your password here'
-tms_databases = ['gizaCARDTMS_2', 'gizaCARDTMSThes_2']
-
-es_user = 'your elasticsearch username here'
-es_password = 'your elasticsearch password here'
-es_cert = 'the path to your certificate here'
