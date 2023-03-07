2ba496627a200f2e3adbcb6d6b99d16489f56078
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-03-07 12:13:48 -0500

    Update webhook matching

--- docker/redeploy.sh
+++ docker/redeploy.sh
@@ -7,13 +7,21 @@
 # timestamps already provided by webhook
 echo "################ Redeploying giza app ################"
 
+# See entire payload so we can differentiate between different events, e.g. push vs Github Actions
+echo $payload
+
+echo "Pulling latest docker images"
 DOCKER_REGISTRY="407196791491.dkr.ecr.us-east-1.amazonaws.com"
 AWS_PROFILE=fasrc_cd aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 
 docker pull $DOCKER_REGISTRY/giza/django:latest
 docker pull $DOCKER_REGISTRY/giza/ingest:latest
 
+echo  "Pulling latest code from git"
 git pull
 
+echo "Stopping and removing old containers"
 docker compose -f docker-compose.yml -f docker-compose-production.yml run --rm django python3 manage.py migrate 
 docker compose -f docker-compose.yml -f docker-compose-production.yml up -d
+
+echo "################ Finished redeploying giza app ################"
--- webhooks/start_webhook.sh
+++ webhooks/start_webhook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/giza/go/bin/webhook -hooks ~/hooks.json -logfile /var/log/webhook/giza-deploy/giza-deploy.log -verbose
+/home/giza/go/bin/webhook -hooks ~/hooks.json -logfile /var/log/webhook/giza-deploy/giza-deploy.log -verbose -hotreload
